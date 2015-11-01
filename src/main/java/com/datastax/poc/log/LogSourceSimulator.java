401e24838883f7be8eafc912668983e44f37d673
patoch <pguillebert@gmail.com>
2015-11-01 12:26:52 +0100

    Reduced sleep time while waiting for termination.

--- src/main/java/com/datastax/poc/log/LogSourceSimulator.java
+++ src/main/java/com/datastax/poc/log/LogSourceSimulator.java
@@ -126,7 +126,7 @@ public class LogSourceSimulator {
         while (!executorService.isTerminated()) {
             System.out.println("Waiting for termination");
             try {
-                Thread.sleep(1000);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
